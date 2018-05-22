e264c343fc023825f6c45361e23f451bc18fdd56
Anytta <30534563+Anytta@users.noreply.github.com>
2018-05-22 17:14:39 +0200

    Update has12.java

--- Array-2/has12.java
+++ Array-2/has12.java
@@ -1,8 +1,6 @@
 /*
-
 Given an array of ints, return true if there is a 1 in the array 
 with a 2 somewhere later in the array.
-
 
 has12([1, 3, 2]) → true
 has12([3, 1, 2]) → true
@@ -13,14 +11,13 @@ public boolean has12(int[] nums) {
   
   for (int i=0; i<nums.length; i++)
   {
-            if ((nums[i]==1))
-            {
-              
-              for (int j=i; j<nums.length; j++)
-              {
-              if ((nums[j]==2)) return true; 
-              }
-            }  
+    if ((nums[i]==1))
+      {
+      for (int j=i; j<nums.length; j++)
+         {
+          if ((nums[j]==2)) return true; 
+         }
+      }  
   }
   return false;
 }
